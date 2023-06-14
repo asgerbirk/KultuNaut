3487d2eae1d8c790773e4d774cee1943dc26dade
breelef <emil_vinther@hotmail.com>
2023-06-14 12:43:38 +0200

    Event component -> detail view

--- components/EventComponent.js
+++ components/EventComponent.js
@@ -15,20 +15,23 @@ export const EventComponent = () => {
 
     const renderItem = ({ item }) => {
         return (
-            <View className="p-1.5 bg-gray-800">
+            <TouchableOpacity onPress={() => navigation.navigate('Detail Event View', { eventId: item.Id })}>
+                <View className="p-1.5 bg-gray-800">
                 <View className="p-2.5 bg-white">
-                    <Text className="font-bold text-sm">{item.Tags + " -"}</Text>
+                <Text className="font-bold text-sm">{item.Tags + " -"}</Text>
                     <View className="flex-row">
                         <Text className="">{item.Startdate + ","}</Text>
                         <Text className="font-bold"> {item.LocationName} </Text>
                     </View>
-                    <Text className="font-bold text-base">{item.Title}</Text>
-                    <Image source={{uri: item.Image}} className="h-44 w-full" resizeMode={"cover"}/>
-                    <TouchableOpacity className="absolute top-4 right-4 bg-gray-400 p-2 rounded" onPress={() => toggleLike(item)}>
+                <Text className="font-bold text-base">{item.Title}</Text>
+                <Image source={{uri: item.Image}} className="h-44 w-full" resizeMode={"cover"}/>
+                    <TouchableOpacity className="absolute top-4 right-4 bg-gray-400 p-2 rounded" onPress={() => toggleLike(item.Id)}>
                         <Icon name={item.isLiked ? "heart" : "heart-o"} size={20} color={item.isLiked ? "#900" : "#000"} />
+
                     </TouchableOpacity>
                 </View>
-            </View>
+                </View>
+            </TouchableOpacity>
         );
     };
 
