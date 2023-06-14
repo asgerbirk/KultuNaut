1df69a81d576717e02f5011c6f0d437cf5cd55bd
asgerbirk <asger.birk@gmail.com>
2023-06-14 13:59:53 +0200

    Works now with navigate

--- components/EventComponent.js
+++ components/EventComponent.js
@@ -1,13 +1,13 @@
+import { useNavigation } from "@react-navigation/native";
+import { useContext } from "react";
 import { Text, View, FlatList, Image, TouchableOpacity } from "react-native";
 import data from "../utils/dummyData";
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { useContext } from "react";
-import {LikedEventsContext} from "../context/LikedEventsContext";
-import {useNavigation} from "@react-navigation/native";
+import { LikedEventsContext } from "../context/LikedEventsContext";
 
 export const EventComponent = () => {
     const navigation = useNavigation();
-    const { likedEvents, toggleLike } = useContext(LikedEventsContext); // Use the context
+    const { likedEvents, toggleLike } = useContext(LikedEventsContext);
 
     // Adds isLiked prop to each item in the data array
     const displayData = data.result.map(item => ({
@@ -27,9 +27,8 @@ export const EventComponent = () => {
                     </View>
                 <Text className="font-bold text-base">{item.Title}</Text>
                 <Image source={{uri: item.Image}} className="h-44 w-full" resizeMode={"cover"}/>
-                    <TouchableOpacity className="absolute top-4 right-4 bg-gray-400 p-2 rounded" onPress={() => toggleLike(item.Id)}>
+                    <TouchableOpacity className="absolute top-4 right-4 bg-gray-400 p-2 rounded" onPress={() => toggleLike(item)}>
                         <Icon name={item.isLiked ? "heart" : "heart-o"} size={20} color={item.isLiked ? "#900" : "#000"} />
-
                     </TouchableOpacity>
                 </View>
                 </View>
