5eea24651a613af8b67f8783f81e8307f4e59e8e
asgerbirk <asger.birk@gmail.com>
2023-06-16 12:43:08 +0200

    Navigation in the bottom done

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,33 +1,58 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import {AllEvents} from "../screens/AllEvents";
 import {DetailView} from "../screens/DetailView";
 import {Home} from "../screens/Home";
 import {EventComponent} from "./EventComponent";
 import {LikedEventsProvider} from "../context/LikedEventsContext";
 import {LikedEventsScreen} from "../screens/LikedEventsScreen";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Stack = createStackNavigator();
-export const Navigation = () => {
+const BottomTab = createBottomTabNavigator()
 
 
-
-     return (
-         <LikedEventsProvider>
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={Home} />
+    function HomeStack() {
+        return (
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                 <Stack.Screen name="Home" component={Home}/>
                 <Stack.Screen name="All upcoming Events" component={AllEvents} />
-                <Stack.Screen name="Detail Event View" component={DetailView}/>
-                
-                <Stack.Screen name="HomeTwo" component={Home} />
-                <Stack.Screen name="Events component" component={EventComponent} />
-                <Stack.Screen name="Liked events" component={LikedEventsScreen} />
+                <Stack.Screen name="Detail Event View" component={DetailView} />
             </Stack.Navigator>
-        </NavigationContainer>
-         </LikedEventsProvider>
-    );
-}
+        );
+    }
+
+
+    export const Navigation = () => {
+        return (
+            <LikedEventsProvider>
+                <NavigationContainer>
+                    <BottomTab.Navigator screenOptions={({ route }) => ({
+                        tabBarIcon: ({ focused, color, size }) => {
+                            let iconName;
+
+                            if (route.name === 'HomeScreen') {
+                                iconName = focused ? 'home' : 'home-outline';
+                            } else if (route.name === 'Events component') {
+                                iconName = focused ? 'calendar' : 'calendar-outline';
+                            } else if (route.name === 'Liked events') {
+                                iconName = focused ? 'heart' : 'heart-outline';
+                            }
+
+                            return <Ionicons name={iconName} size={size} color={color} />;
+                        },
+
+                    })}>
+                        <BottomTab.Screen name="HomeScreen" component={HomeStack} options={{ tabBarLabel: 'Hjem', headerTitle: ""}} />
+                        <BottomTab.Screen name="Events component" component={EventComponent} options={{ tabBarLabel: 'Kommende begivenheder', headerTitle: "" }} />
+                        <BottomTab.Screen name="Liked events" component={LikedEventsScreen} options={{ tabBarLabel: 'Dine begivenheder', headerTitle: "" }} />
+                    </BottomTab.Navigator>
+                </NavigationContainer>
+            </LikedEventsProvider>
+        );
+    }
+
 
 
