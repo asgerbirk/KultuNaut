751b7eb24afe7ec986db89af070f488e77c12e3c
asgerbirk <asger.birk@gmail.com>
2023-06-18 10:16:27 +0200

    Gjorde så alle de nyese events kommer op først ved at bruge reverse

--- screens/EventScreen.js
+++ screens/EventScreen.js
@@ -21,7 +21,7 @@ export const EventScreen = () => {
     }, []);
 
     useEffect( () => {
-        if (token) { 
+        if (token) {
             const url = "https://www.kultunaut.dk/perl/api2/EventLonLatDate?lat=55.6401&lon=12.0741&radius=1000&page=1&pagesize=30&fieldlist=Startdate"
 
             fetch(url, {
@@ -39,12 +39,12 @@ export const EventScreen = () => {
                 console.error('Error:', error);
             });
         }
-    }, [token]); 
+    }, [token]);
 
 
     useEffect(() => {
         const url = `https://www.kultunaut.dk/perl/api2/EventId?Id=${ids}`;
-    
+
         fetch(url, {
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -52,12 +52,13 @@ export const EventScreen = () => {
         })
             .then(response => response.json())
             .then(data => {
-                if(data.result) { 
+                if(data.result) {
                     const dataToDisplay = data.result.map(item => ({
                         ...item,
                         isLiked: likedEvents.some(likedEvent => likedEvent.Id === item.Id)
                     }));
-                    setDisplayData(dataToDisplay);
+                    setDisplayData([...dataToDisplay].reverse());
+
                 }
             })
             .catch(error => {
@@ -76,8 +77,11 @@ export const EventScreen = () => {
     return (
         <FlatList
             data={displayData}
-            renderItem={({ item }) => <EventComponent item={item} />} 
+            renderItem={({ item }) => <EventComponent item={item} />}
             keyExtractor={(item) => item.Id.toString()}
         />
     );
 }
+
+
+
